8c092c81e39b3df3591b271e835cfb51ccc4a768
lemdorot <lemdorot@gmail.com>
2022-08-17 21:51:51 +0300

    Сделал вторую задачу по программированию

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Первая задача
 const getMultiplicationTable = (num) => {
   if (typeof num === 'number') {
     const numbersTable = [];
@@ -70,3 +71,96 @@ const showTable = (num) => {
 };
 
 showTable(10);
+
+// Вторая задача
+
+const arrayNumbers = [41, 55, 61, 1, 8, 27, 37, 39];
+
+const arraySort = (arr) => arr.slice().sort((a, b) => a - b);
+
+const arrayFilter = (arr) => {
+  let array = arr;
+  let isMatch = false;
+  let isComplete = false;
+  let arrMatch = [];
+  while (true) {
+    if (array.length <= 1) {
+      break;
+    }
+    for (let i = 0; i < array.length - 1; i += 1) {
+      // разбиваем каждое число на массив цифр
+      const arrNum = Array.from(array[i].toString());
+      // берем по очереди каждую цифру из этого числа
+      for (let j = 0; j < arrNum.length; j += 1) {
+        // проходим по массиву чисел
+        for (let k = i + 1; k < array.length; k += 1) {
+          // проверяем включение цифры в каждом числе массива
+          if (Array.from(array[k].toString()).includes(arrNum[j])) {
+            // добавляем такие числа в массив
+            arrMatch.push(array[k]);
+            isMatch = true;
+          }
+        }
+      }
+      // если совпадения были
+      if (isMatch) {
+        // добавляем число с которым сравнивали
+        arrMatch.push(array[i]);
+        const set = new Set(arrMatch);
+        // удаляем из массива эти числа
+        array = array.filter((item) => !set.has(item));
+        arrMatch = [];
+        isMatch = false;
+        break;
+      }
+      isComplete = true;
+    }
+    if (isComplete) break;
+  }
+  console.log(array);
+  return array;
+};
+
+// разворачиваем каждое число массива
+const reversNumbers = (arr) => {
+  const array = arr;
+  const newArr = [];
+  for (let i = 0; i < array.length; i += 1) {
+    newArr.push(Number(Array.from(array[i].toString()).reverse().join('')));
+  }
+  console.log(newArr);
+  return newArr;
+};
+
+// возводим в квадрат каждое число массива
+const sqrNumbers = (arr) => {
+  const array = arr;
+  const newArr = [];
+  for (let i = 0; i < array.length; i += 1) {
+    newArr.push(array[i] * array[i]);
+  }
+  return newArr;
+};
+
+const getArray = (arr) => {
+  let result = arr;
+  const arrLength = arr.length;
+
+  while (true) {
+    // сортируем массив
+    result = arraySort(result);
+    // фильтруем массив
+    result = arrayFilter(result);
+    // если количество элементов 1 или 0 или количество элементов не изменилось, то завершаем цикл
+    if (result.length <= 1 || arrLength === result.length) {
+      break;
+    } else {
+      // иначе переворачиваем числа
+      result = reversNumbers(result);
+      // и возводим в квадрат
+      result = sqrNumbers(result);
+    }
+  }
+};
+
+getArray(arrayNumbers);
